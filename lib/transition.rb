e7de9c1a32aca2659ab9b9529f6af2dbe89ec5d8
Fernando Batista <fernandohsbatista@gmail.com>
2024-03-01 08:33:03 -0300

    fix failing test that ensures context creates a transition object when moving to next state

--- lib/transition.rb
+++ lib/transition.rb
@@ -1,8 +1,17 @@
 class Transition
+  attr_reader :context, :start_state, :end_state, :time
+
   def initialize(context:, start_state:, end_state:, time:)
     @context = context
     @start_state = start_state
     @end_state = end_state
     @time = Time.at(time.to_i)
+  end
+
+  def ==(other)
+    self.context == other.context &&
+    self.start_state == other.start_state &&
+    self.end_state == other.end_state &&
+    self.time == other.time
   end
 end
