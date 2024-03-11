1bd0c27ddb46f2d02f7e1980c600d3855f5460f0
Fernando Batista <fernandohsbatista@gmail.com>
2024-03-10 23:07:37 -0300

    ensure EmailValidation sends token on initialization

--- spec/states/email_validation_spec.rb
+++ spec/states/email_validation_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe States::EmailValidation do
     user = User.new(phone: '999 999', email: nil)
     expect { user.check_sms_token('1234') }.to raise_error(States::Errors::InvalidStateToTransition)
   end
+
+  it "sends token on initialization" do
+    user = User.new(phone: '999 999', email: 'email@email.com')
+    user.state = States::EmailValidation.new(user)
+    expect(user.email_token).not_to eq(nil)
+  end
 end
