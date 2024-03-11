3bd3c612b2bae2f1ca7313aa6b22dea2e4535336
Fernando Batista <fernandohsbatista@gmail.com>
2024-03-10 22:01:26 -0300

    remove unnecessary test

--- spec/states/sms_validation_spec.rb
+++ spec/states/sms_validation_spec.rb
@@ -4,13 +4,6 @@ require "states/errors"
 require "user"
 
 RSpec.describe States::SmsValidation do
-  it "transitions user to SmsValidation state" do
-    user = User.new
-    user.state = States::SmsValidation
-    user.next_state
-    expect(user.state).to eq(States::EmailValidation)
-  end
-
   it "cannot transition to SmsValidation if there is no phone number" do
     user = User.new
     user.phone = nil
