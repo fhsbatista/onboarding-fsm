3308390a1dea5198233d5b8a0388bbe176eb447c
Fernando Batista <fernandohsbatista@gmail.com>
2024-03-16 20:58:59 -0300

    refactor transitions logging specs

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -9,14 +9,7 @@ RSpec.describe User do
   end
 
   describe "on state transition" do
-    it "to SmsValidation should log" do
-      user = User.new(phone: "999 999")
-      log = user.transitions_logs.first
-      expect(log.previous_state).to be_nil
-      expect(log.current_state).to be_a(States::SmsValidation)
-    end
-
-    it "to EmailValidation should log" do
+    it "should log" do
       user = User.new(phone: "999 999", email: "test@test.com")
       user.transition_state(States::EmailValidation.new(user))
       log = user.transitions_logs[1]
