358cd01b524bc253a409d18876a58bf9642aeae5
Fernando Batista <fernandohsbatista@gmail.com>
2024-03-16 21:07:11 -0300

    ensure logs the current time on a state transition

--- lib/transition_log.rb
+++ lib/transition_log.rb
@@ -1,9 +1,10 @@
 class TransitionLog
-  attr_reader :previous_state, :current_state
+  attr_reader :previous_state, :current_state, :date_time
 
   def initialize(previous_state:, current_state:)
     @previous_state = previous_state
     @current_state = current_state
+    @date_time = Time.now
   end
 
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -13,8 +13,13 @@ RSpec.describe User do
       user = User.new(phone: "999 999", email: "test@test.com")
       user.transition_state(States::EmailValidation.new(user))
       log = user.transitions_logs[1]
+
       expect(log.previous_state).to be_a(States::SmsValidation)
+
       expect(log.current_state).to be_a(States::EmailValidation)
+
+      secs_tolerance = 1
+      expect(log.date_time).to be_within(secs_tolerance).of(Time.now)
     end
   end
 end
