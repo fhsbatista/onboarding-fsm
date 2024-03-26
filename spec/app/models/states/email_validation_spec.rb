d14b8f68f07add0868ca5231511315501d69b3bf
Fernando Batista <fernandohsbatista@gmail.com>
2024-03-26 08:53:43 -0300

    ensure user transitions to SendSelfie when email_token is valid

--- spec/app/models/states/email_validation_spec.rb
+++ spec/app/models/states/email_validation_spec.rb
@@ -21,4 +21,14 @@ RSpec.describe States::EmailValidation do
     user.transition_state(States::EmailValidation.new(user))
     expect { user.send_selfie }.to raise_error(States::Errors::InvalidEvent)
   end
+
+  describe "check sms token" do
+    it "transitions to SendSelfie state when token is valid" do
+      user = Users::Entity.new(phone: "999 999", email: "email@email.com")
+      user.transition_state(States::EmailValidation.new(user))
+      valid_token = user.email_token
+      user.check_email_token(valid_token)
+      expect(user.state).to be_a(States::SendSelfie)
+    end
+  end
 end
